dfed09866306fc3d1d13956d4f4414b680ef4480
Nathan <frost.nathan1995@gmail.com>
2022-06-19 22:00:16 -0400

    finished queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,17 @@
--- write your queries here
+-- 1.
+SELECT * FROM owners
+FULL OUTER JOIN vehicles
+ON owners.id=vehicles.owner_id;
+
+-- 2.
+SELECT first_name, last_name, COUNT(owner_id) FROM owners
+JOIN vehicles ON owners.id=vehicles.owner_id
+GROUP BY (first_name, last_name)
+ORDER BY first_name;
+
+-- 3.
+SELECT first_name, last_name, ROUND(AVG(price)) as avg_price, COUNT(owner_id) FROM owners
+JOIN vehicles on owners.id=vehicles.owner_id
+GROUP BY (first_name, last_name)
+HAVING COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000
+ORDER BY first_name DESC;
